d93b739b9f5ba10c9e535705e1a15e1dd8710eb7
SimonDCT <144129063+SimonDCT@users.noreply.github.com>
2023-09-05 16:57:28 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import button from './component/button';
+
+function App() {
+  return <><p>Hello</p></>;
+}
+
+export default App;
---
+++ src/component/variable.jsx
@@ -0,0 +1,8 @@
+import './variable.css';
+
+export default function variable({label}) {
+  return(
+    <>
+    <span> className="label"> {label} </span>;
+  );
+};
